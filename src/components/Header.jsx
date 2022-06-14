3fc6b29caba6dfdb149e267bc0b36e69fef892ac
bhaven01-geek <65948166+bhaven01-geek@users.noreply.github.com>
2022-06-14 19:37:29 +0530

    made mobile responsive and changed fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ import SlipLayerImg from '../Assets/tempatesoverlays.png';
 import LadyImg from '../Assets/ladylaptop.png';
 import Vector1Img from '../Assets/layout.svg';
 import Vector2Img from '../Assets/layout2.svg';
+import UserVector from '../Assets/user.svg';
+import ArrowRight from '../Assets/arrow-right-circle.svg';
 const Header = () => {
     return (
         <>
@@ -17,28 +19,34 @@ const Header = () => {
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ml-lg-5 pl-lg-4">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0 ml-lg-">
+                            <li className="nav-item px-2">
+                                <a className="nav-link" href="#intro" >Home <span className="sr-only">(current)</span></a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
+                            <li className="nav-item px-2">
+                                <a className="nav-link" href="#features">Features</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item px-2">
                                 <a className="nav-link" href="#">Help</a>
                             </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contact</a>
+                            <li className="nav-item px-2">
+                                <a className="nav-link" href="#footer">Contact Us</a>
                             </li>
                         </ul>
 
                         <ul id="request-head" className="navbar-nav mr-0 mt-2 mt-lg-0">
-                            <li className="nav-item d-none d-md-block">
-                                <button id="login-btn" type="button" className="btn btn-lg text-white font-weight-bold ">Login</button>
+                            <li className="nav-item d-none d-md-block mr-4">
+                                <button id="login-btn" type="button" className="btn btn-lg text-white font-weight-bold ">
+                                {/* <i class="fa fa-user-f"></i> */}
+                                <img src={UserVector} alt="User" class="img-user" />
+                                Login
+                                </button>
                             </li>
 
-                            <li className="nav-item d-none d-md-block">
-                                <button id="try-free-btn" type="button" className="btn btn-lg text-black  font-weight-bold ">Try For Free</button>
+                            <li className="nav-item d-none d-md-block  ">
+                                <button id="try-free-btn" type="button" className="btn btn-lg-0 text-black  font-weight-bold ">
+                                Try For Free
+                                    </button>
                             </li>
 
                         </ul>
@@ -59,10 +67,16 @@ const Header = () => {
                     <div className="col-md-10 offset-md-1 col-xl-10 offset-xl-0 double-gutter-left-padding ">
                         <h2 id="business-tagline" className="text-center text-md-left">Reward your Employees in Just <span className="business-tag" >one click</span></h2>
                         <p className="text-center  text-md-left color-grayish-blue">The world has changed with digitization and automation.
-                            And so are your HR working strategies.Redefine your payroll system that automates and helps you save time.
-                            Manage leave and salary instantly.</p>
+                         And so are your HR working strategies.
+                         Redefine your payroll system that automates and helps you save time.
+                         Manage leave and salary instantly.
+                         Turn data into smarter decisions and create experiences your employees will love.</p>
                         <div class="box-container">
-                            <button id="try-slip-btn" type="button" class="btn btn-lg rounded-pill">Try ZapSlips</button>
+                            <button id="try-slip-btn" type="button" class="btn btn-lg rounded-pill">
+                                
+                                Try ZapSlips
+                                <img src= {ArrowRight} alt ="right-arrow" class = "arrow-img"/>
+                                </button>
                         </div>
                     </div>
                 </div>
@@ -70,20 +84,21 @@ const Header = () => {
 
 
             {/* <!-- Features --> */}
-            <div id="choose-easybank" class="d-flex container-md mt-5 pt-5 mt-md-custom-easybank flex-direction-column flex-direction-md-row flex-wrap flex-wrap-md-nowrap">
+            <div id="features" class="d-flex container-md mt-5 pt-5 mt-md-features-section flex-direction-column flex-direction-md-row flex-wrap flex-wrap-md-nowrap">
 
                 <div id="cards" class="d-sm-flex justify-content-sm-center align-items-sm-start col-md-10 offset-md-1 flex-sm-wrap">
-                    <h2 class="text-center text-md-left w-100 gutter-md-left-padding">Easily Customize Your Payroll</h2>
+                    <h2 class="text-center text-md-left w-100 gutter-md-left-padding">Easily Customize 
+                    Your Payroll</h2>
                     {/* <p class="text-center text-md-left  w-100 gutter-md-left-padding">Overcome the complexities and risks associated with sourcing, managing with and delivering payroll services with standardisation and accuracy.</p> */}
                     <div class="row" id="feature-cards">
                         <div id="Card-Feat-1" class="card col-sm-6 col-lg-3 row justify-content-center align-items-center  flex-direction-column">
-                            <img src={Vector1Img} alt="Accurate and Simple" class="img-fluid" />
+                            <img src={Vector1Img} alt="Accurate and Simple" class="img" />
                             <h4>Accurate and Simple</h4>
                             <p> Overcome the complexities and risks associated with sourcing,
                                 managing with and delivering payroll services with standardisation and accuracy</p>
                         </div>
                         <div id="Card-Feat-2" class="card col-sm-6 col-lg-3 row justify-content-center align-items-center  flex-direction-column">
-                            <img src={Vector2Img} alt="Best Customisable payroll system" class="img-fluid" />
+                            <img src={Vector2Img} alt="Best Customisable payroll system" class="img" />
                             <h4>Best Customisable payroll system</h4>
                             <p>Self configurable and highly customizable software that meets your
                                 end-to-end payroll needs.</p>
@@ -91,13 +106,13 @@ const Header = () => {
                     </div>
                     <div class="row" id="feature-cards">
                         <div id="Card-Feat-3" class="card col-sm-6 col-lg-3 row justify-content-center align-items-center  flex-direction-column">
-                            <img src={Vector2Img} alt="Payroll services tailored to match the needs. " class="img-fluid" />
+                            <img src={Vector2Img} alt="Payroll services tailored to match the needs. " class="img" />
                             <h4>Payroll services tailored to match the needs. </h4>
                             <p>Whatever your number of employees,
                                 just reward them in a single click.</p>
                         </div>
                         <div id="Card-Feat-4" class="card col-sm-6 col-lg-3 row justify-content-center align-items-center  flex-direction-column">
-                            <img src={Vector1Img} alt="Simple statutory compliance" class="img-fluid" />
+                            <img src={Vector1Img} alt="Simple statutory compliance" class="img" />
                             <h4>Simple statutory compliance</h4>
                             <p>Keep your company out of trouble with the law. We handle all of your statutory compliance (PF, PT, ESI, LWF, and IT)
                                 and make tax reporting simple with tax reports. </p>
@@ -109,7 +124,7 @@ const Header = () => {
 
             {/* <!-- Features --> */}
 
-            <div id="intro" className="d-flex flex-direction-column flex-direction-md-row flex-wrap flex-wrap-md-nowrap">
+            <div id="explores" className="d-flex flex-direction-column flex-direction-md-row flex-wrap flex-wrap-md-nowrap">
                 <section class="section">
 
                     <div class="section-container">
@@ -120,7 +135,7 @@ const Header = () => {
                             </p>
                             <div id="explore-cards" class="d-sm-flex justify-content-sm-start align-items-sm-start row-md-5  flex-sm-wrap">
 
-                                <div className='explore-card-1' class="explore-card col-sm-6 col-lg-3 row justify-content-center align-items-center  flex-direction-column">
+                                <div className='explore-card-1' class="explore-card col-sm-3 col-lg-3 row justify-content-center align-items-center  flex-direction-column">
                                     <h4>Smart Collaboration</h4>
                                 </div>
 
@@ -145,8 +160,7 @@ const Header = () => {
 
 
             {/* footer */}
-            <div id="" className="">
-                <footer class="footer">
+                <footer class="footer" id ="footer">
                     <div class="container">
                         <div class="row">
                             <div class="col-md-6 col-lg-4">
@@ -269,7 +283,6 @@ const Header = () => {
                         </div>
                     </div>
                 </footer>
-            </div>
         </>
     )
 
